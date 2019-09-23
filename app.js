f3d0a74c927dc782649bb2009d609c4ae04a52a6
Lindy <lindy.wwh@gmail.com>
2019-09-23 19:26:31 +0100

    backend

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: "remotemysql.com",
+    user: "7D5oLoh1Ft",
+    password: "zR1K1CNQkG",
+    database:"7D5oLoh1Ft"
+});
+
+db.connect();
+
+app.get('/data', function(req,res){
+var sql = 'SELECT * FROM emails';
+db.query(sql, (err, result)=>{
+    if(err) throw err;
+    console.log(result);
+    res.send(result);
+});
+});
+
+app.post('/data', function(req, res){
+    var area = req.body.area;
+    console.log("AREAS " + req.body.area);
+    var query = `SELECT email FROM emails WHERE area="${area}"`;
+
+    db.query(query, function(err, result)
+    {
+        if (err)
+        {
+            throw err;
+        }
+        else 
+        {
+            if (result.length>0)
+            {
+                res.send(result);
+            }
+        }
+    });
+
+});
+
+app.listen(3210, ()=>{
+    console.log('Server connected to port 3210')
+});
